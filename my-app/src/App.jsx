3a95dcd3ad6de8a4ec5e9ac17e52788b5049f3a5
Kazu-pnj <gustavokmatsunaka1@gmail.com>
2026-03-06 17:13:46 +0000

    Termino-da-Atividade-do-Dia-04-03-26

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,14 +2,21 @@ import './App.css'
 import Titulo from './components/Titulo'
 import Aluno from './components/Aluno'
 import Botao from './components/Botao'
+import Texto from './components/Texto'
 
 function App() {
   return (
     <div>
       <h1>Minha Primeira Aula! YEEY</h1>
+
       <Titulo/>
+
       <Aluno/>
-      <Botao/>
+
+      <button onClick={Botao}>Clique aqui!</button><br></br>
+
+      <input type = "text" onChange={Texto}/>
+
     </div>
   )
 }
--- my-app/src/components/Botao.jsx
+++ my-app/src/components/Botao.jsx
@@ -2,8 +2,6 @@ function Botao() {
     alert("Boas vindas Aluno muito Bacana")
 }
 
-return (
-    <button onClick={Botao}>Botão muito bacana</button>
-)
+<button onClick={Botao}>Botão muito bacana</button>
 
 export default Botao
